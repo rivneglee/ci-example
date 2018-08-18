d0cc2f275947e1c411cd70f1e28acb80de73b8e5
Mengjiao Li <mjli@CNmjli.local>
2018-08-18 09:55:59 +0800

    modify

--- src/main/java/com/thoughtworks/nho/ci/example/services/MessageService.java
+++ src/main/java/com/thoughtworks/nho/ci/example/services/MessageService.java
@@ -5,6 +5,6 @@ import org.springframework.stereotype.Service;
 @Service
 public class MessageService {
    public String getGreetingMessage() {
-       return "欢迎加入TW!! Red  ！ ";
+       return "欢迎加入TW!! Red  ！！！";
    }
 }
